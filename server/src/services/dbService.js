fed29302ca5f404f7ac9262b0ae9fbee40ef1ac9
Samuel Brown <samuelwbrown4@gmail.com>
2026-07-31 09:49:12 -0400

    more logs added for errors

--- server/src/services/dbService.js
+++ server/src/services/dbService.js
@@ -2,13 +2,14 @@ const {pools} = require('../db/connections');
 
 const executeQuery = async(source , query) => {
     try{
-        
+        console.log('>>> EXECUTING QUERY FOR SOURCE:', source)
         if(!pools[source]){
             throw new Error('No connection exists for source')
         }
         let response = await pools[source].query(query)
         return response.rows
     }catch(error){
+        console.log('>>> CAUGHT ERROR IN executeQuery:', error)
         console.log(error)
         throw error
     }
