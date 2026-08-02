7074bd2a8d9dd68234a72e550beb42521b44632a
Samuel Brown <samuelwbrown4@gmail.com>
2026-08-01 22:17:28 -0400

    build out frontend, add cors to backend

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import { Input, Select, Image } from '@mantine/core'
+import styles from './Home.module.css'
+
+function Home() {
+    const [queryInput, setQueryInput] = useState('');
+    const [source , setSource] = useState('');
+
+    const [answer , setAnswer] = useState('')
+
+    const API_URL = import.meta.env.VITE_API_URL
+
+    async function submitQuery(source , queryInput){
+        if(source === '' || queryInput === ''){
+            return alert('Need to select a data source and submit a query')
+        }
+        try{
+            let response = await fetch(`${API_URL}/api/query` , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    source: source,
+                    query: queryInput
+                })
+            })
+
+            let result = await response.json()
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    return (
+        <div className={styles.root}>
+            <div style={{flex: 1 , marginTop: '3rem' , display: 'flex' , flexDirection: 'column' , alignItems: 'center'}}>
+                <h1 style={{ color: 'white' , paddingBottom: '2rem' , margin: '0rem' }}>Welcome to SchemaSpeak!</h1>
+            
+            <div style={{paddingBottom: '2rem' , display: 'flex' , alignItems: 'center'}}>
+                <Image src={'/schema_speak_logo.png'} h={150} w={150}/>
+            </div>
+            </div>
+            <div className={styles.inputDiv}>
+                
+                <form>
+                    <div style={{ display: 'flex', justifyContent: 'center', width: '100%', alignItems: 'center', gap: '1rem' }}>
+                        <div style={{ width: '25%' }}>
+                            <Select 
+                                styles={{ 
+                                    input: { 
+                                        backgroundColor: '#333', 
+                                        border: '1px solid white', 
+                                        width: '100%', 
+                                        borderRadius: '30px' , 
+                                        color: 'white' 
+                                    }, 
+                                    wrapper: {
+                                        width: '100%'
+                                    } 
+                                }} 
+                                size='xl' 
+                                data={['Routebase (TMS)']} 
+                                value={source} 
+                                onChange={(value)=>setSource(value)} 
+                                w={230} 
+                                placeholder="Select Source" />
+                        </div>
+                        <div style={{ width: '75%' }}>
+                            <Input size='xl' styles={{ input: { width: '100%', backgroundColor: '#333', borderColor: 'white', color: 'white', borderRadius: '30px' }, wrapper: { width: '100%' } }} value={queryInput} onChange={(e) => setQueryInput(e.target.value)} placeholder={'Ask away...'} />
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div style={{flex: 1}}>
+                <h1>*results here*</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
 
@@ -6,6 +7,7 @@ const PORT = 3000
 
 const queryRoute = require('./src/routes/query')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api' , queryRoute)
