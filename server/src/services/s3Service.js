e0919fe9571a33d558cfb0e09be13010b9106783
Samuel Brown <samuelwbrown4@gmail.com>
2026-08-09 21:01:11 -0400

    logging file key for debugging

--- server/src/services/s3Service.js
+++ server/src/services/s3Service.js
@@ -3,6 +3,7 @@ const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
 const s3Client = new S3Client({ region: 'us-east-1' })
 
 const getDocContent = async (key) => {
+    console.log('Fetching key:', key)
     const command = new GetObjectCommand({
         Bucket: 'schemaspeak-docs',
         Key: key
