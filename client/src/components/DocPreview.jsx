09162cee03fb75a1faedd1986ab7fff048a09aea
Samuel Brown <samuelwbrown4@gmail.com>
2026-08-13 01:25:13 -0400

    changes to preview route to send html for docx using same get buffer, extract text , html conversion pipeline as done for file upload

--- client/src/components/DocPreview.jsx
+++ client/src/components/DocPreview.jsx
@@ -1,11 +1,15 @@
 import {Modal} from '@mantine/core'
 import ReactMarkdown from 'react-markdown'
+import DOMPurify from 'dompurify'
 
 function DocPreview({file , showPreview , setShowPreview , fileName}){
     return(
         <Modal opened={showPreview} onClose={()=>setShowPreview(false)} size='xl' title={`Previewing: ${fileName}`}>
             <div>
-                <ReactMarkdown>{file}</ReactMarkdown>
+                {fileName.split('.').pop() !== 'docx' && <ReactMarkdown>{file}</ReactMarkdown>}
+                {fileName.split('.').pop() === 'docx' && 
+                    <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(file) }}  style={{color: 'white' , overflowY: 'scroll'}}/>
+                }
             </div>
         </Modal>
     )
--- server/src/services/docListService.js
+++ server/src/services/docListService.js
@@ -1,4 +1,6 @@
-const {getDocConfig , getDocContent} = require('../services/s3Service')
+const {getDocConfig , getDocContent , getDocBuffer} = require('../services/s3Service')
+const {extractText} = require('../services/docCreateService')
+const mammoth = require('mammoth')
 
 const getDocListService = async (req , res) => {
     try{
@@ -13,7 +15,18 @@ const getDocListService = async (req , res) => {
 const previewDocService = async (req , res) => {
     try{
         const {file} = req.params
+        let buffer = await getDocBuffer(file)
+        let extractedText = await extractText(file , buffer)
+
+        let previewHtml = null
+        if (file.split('.').pop() === 'docx') {
+            const result = await mammoth.convertToHtml({ buffer: buffer})
+            previewHtml = result.value
+        }
         let content = await getDocContent(file)
+        if(previewHtml){
+            return res.status(200).json(previewHtml)
+        }
         res.status(200).json(content)
 
     }catch(error){
